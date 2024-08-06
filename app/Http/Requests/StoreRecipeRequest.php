370ac094f96d959b3e2f921214179f2dc1ba9f42
Lum Beqiri <lumbeqiri@live.com>
2024-08-06 21:47:13 +0200

    some fixes for model binding and migration fixes

--- app/Http/Requests/StoreRecipeRequest.php
+++ app/Http/Requests/StoreRecipeRequest.php
@@ -25,6 +25,7 @@ class StoreRecipeRequest extends FormRequest
             'title' => 'required|string|max:255',
             'content' => 'required|string',
             'image' => 'nullable|image|max:2048',
+            // TODO: the frontend only has access to the uuid not the id, so we need to change this to uuid in the rules part
             'category_id' => 'required|exists:categories,id',
         ];
     }
